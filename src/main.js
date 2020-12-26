f10d7775ff696b1d7d1c9052a0b6c1faed73226d
shinn <matsuzaki@startup-technology.com>
2020-12-26 21:02:09 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -6,18 +6,6 @@ import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyAJzU9RkpS7nUFQAM-T6_Y-HL_qZ0QLbBM",
-    authDomain: "vue-todo-3c7e4.firebaseapp.com",
-    projectId: "vue-todo-3c7e4",
-    storageBucket: "vue-todo-3c7e4.appspot.com",
-    messagingSenderId: "189618802022",
-    appId: "1:189618802022:web:9ddc26cb501dd296240ca7"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
 new Vue({
   vuetify,
   router,
